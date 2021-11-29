c1ea8491ae7e4fa21849d9d80763293f005c3c44
lucasABLima <lucasablima99@gmail.com>
2021-11-29 20:16:34 -0300

    Add delete/update Prof

--- implementacao/app/controllers/professor.js
+++ implementacao/app/controllers/professor.js
@@ -60,5 +60,50 @@ module.exports = (app) => {
       }
     }
 
+    professor.getById = async (req,res) => {
+
+      try{
+        const professor = await Professor.findOne({ raw: true, where: { id: req.params.id } });
+        return res.render('professor/new', { professor: professor })
+      }catch(err){
+        return res.status(400).send({ error: 'Bad Request' });
+      }
+    }
+
+    professor.update = async (req,res) => {
+      const { email, senha, nome, cpf, instituicaoEnsino, departamento, carteira } = req.body;
+
+      try{
+        await Professor.update({
+          email: email, 
+          senha: senha, 
+          //role: role,
+          nome: nome,
+          cpf: cpf,
+          instituicaoEnsino: instituicaoEnsino,
+          departamento: departamento,
+          carteira: carteira
+        }, { where: { id: req.params.id }})
+
+        return res.redirect('/')
+      }catch(err){
+        console.log(err)
+        return res.status(400).send({ error: 'Bad Request' });
+      }
+    }
+
+    professor.delete = async (req,res) => {
+      try{
+        Professor.destroy({
+          where: {
+              id: req.params.id
+          }
+        })
+        return res.redirect('/')
+      }catch(err){
+        return res.status(400).send({ error: 'Bad Request' });
+      }
+    }
+
     return professor;
   }
