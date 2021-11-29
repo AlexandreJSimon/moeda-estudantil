97523ade51b2e6d87fb450009138c4ea3f6b0a75
lucasABLima <lucasablima99@gmail.com>
2021-11-29 19:46:53 -0300

    Add delete/update empr

--- implementacao/app/controllers/empresa.js
+++ implementacao/app/controllers/empresa.js
@@ -58,5 +58,49 @@ module.exports = (app) => {
       }
     }
 
+    empresa.getById = async (req,res) => {
+
+      try{
+        const empresa = await Empresa.findOne({ raw: true, where: { id: req.params.id } });
+        return res.render('empresa/new', { empresa: empresa })
+      }catch(err){
+        return res.status(400).send({ error: 'Bad Request' });
+      }
+    }
+
+    empresa.update = async (req,res) => {
+      const { email, senha, nome, descricao, valor } = req.body;
+
+      try{
+        await Empresa.update({
+          email: email, 
+          senha: senha, 
+          //role: role,
+          nome: nome,
+          descricao: descricao,
+          rg: rg,
+          valor: valor,
+        }, { where: { id: req.params.id }})
+
+        return res.redirect('/')
+      }catch(err){
+        console.log(err)
+        return res.status(400).send({ error: 'Bad Request' });
+      }
+    }
+
+    empresa.delete = async (req,res) => {
+      try{
+        Empresa.destroy({
+          where: {
+              id: req.params.id
+          }
+        })
+        return res.redirect('/')
+      }catch(err){
+        return res.status(400).send({ error: 'Bad Request' });
+      }
+    }
+
     return empresa;
   }
